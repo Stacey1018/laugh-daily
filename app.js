d5545d3714d8369c970536d7b524704d0cc7c5f2
zhangyang <zhangyang@lingkedsee.com>
2020-06-16 16:50:53 +0800

    连接mongodb数据库

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const logger = require('koa-logger')
 const index = require('./routes/index')
 const users = require('./routes/users')
 
+const config = require('./dbs/config.js')
 // error handler
 onerror(app)
 
@@ -35,6 +36,7 @@ app.use(async (ctx, next) => {
 // routes
 app.use(index.routes(), index.allowedMethods())
 app.use(users.routes(), users.allowedMethods())
+
 
 // error-handling
 app.on('error', (err, ctx) => {
